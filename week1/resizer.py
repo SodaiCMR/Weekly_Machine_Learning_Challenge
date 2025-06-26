30e47f92029713daa1f55b63143e25f5cc80cb6d
Daryl <bragouel@gmail.com>
2025-06-26 13:38:59 +0200

    week1 : - quick change in resizer.py

--- week1/resizer.py
+++ week1/resizer.py
@@ -5,17 +5,15 @@ directory = "datasets"
 size = (300, 300)
 
 for subdir in os.listdir(directory):
-    if subdir != "Cid_Kagenou":
-        folder = os.path.join(directory, subdir)
-        print(folder)
-        for character in os.listdir(folder):
-            img_path = os.path.join(folder, character)
-            img_desc = img_path.split("\\")[-1]
-            original_image = Image.open(img_path)
-            resized_path = os.path.join(folder, f"resized_{img_desc}")
-            if "resized_" not in img_path:
-                converted_image = original_image.convert("RGB")
-                resized_image = converted_image.resize(size)
-                resized_image.save(resized_path)
-                print(f"Successfully resized {img_desc}")
-                os.remove(img_path)
+    folder = os.path.join(directory, subdir)
+    for character in os.listdir(folder):
+        img_path = os.path.join(folder, character)
+        img_desc = img_path.split("\\")[-1]
+        original_image = Image.open(img_path)
+        resized_path = os.path.join(folder, f"resized_{img_desc}")
+        if "resized_" not in img_path:
+            converted_image = original_image.convert("RGB")
+            resized_image = converted_image.resize(size)
+            resized_image.save(resized_path)
+            print(f"Successfully resized {img_desc}")
+            os.remove(img_path)
